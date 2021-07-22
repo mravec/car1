c0eb178cc8713bc72fffc9b9bb94a2f4658d58ec
mravec <48887485+mravec@users.noreply.github.com>
2021-07-22 16:22:38 +0200

    Obnoviť 6. 7. 2021, 10:02:35 chyby posledne

--- main.ts
+++ main.ts
@@ -11,19 +11,11 @@ input.onButtonPressed(Button.A, function () {
         . . # . .
         `)
 })
-<<<<<<< HEAD
-function vypis () {
-    aktualny_cas = input.runningTime()
-    if (aktualny_cas - stary_cas > 1000) {
-        stary_cas = aktualny_cas
-        serial.writeLine("rychlost" + rychlost + ", kompas=" + kompas)
-=======
 function vypis (text: string) {
     s += 1
     if (s > 1) {
         s = 0
         serial.writeLine("lavyMotor" + lavyMotor + ", pravyMotor=" + pravyMotor + ", time=" + input.runningTime() + ", prekazka=" + prekazka + text)
->>>>>>> parent of f7dcba5 (Aktualizovať pxt.json, main.blocks, main.ts)
     }
 }
 input.onButtonPressed(Button.B, function () {
@@ -53,17 +45,10 @@ input.onLogoEvent(TouchButtonEvent.Touched, function () {
     cuteBot.motors(0, 0)
     soundExpression.spring.play()
 })
-<<<<<<< HEAD
-let kompas = 0
-let stary_cas = 0
-let aktualny_cas = 0
-let rychlost = 0
-=======
 let prekazka = 0
 let s = 0
 let pravyMotor = 0
 let lavyMotor = 0
->>>>>>> parent of f7dcba5 (Aktualizovať pxt.json, main.blocks, main.ts)
 serial.redirectToUSB()
 radio.setGroup(1)
 basic.showLeds(`
@@ -78,10 +63,6 @@ pravyMotor = 0
 s = 0
 soundExpression.happy.play()
 basic.forever(function () {
-<<<<<<< HEAD
-    cuteBot.motors(0, -65)
-    basic.pause(100)
-=======
     prekazka = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
     if (prekazka < 30 && (lavyMotor > 0 || pravyMotor > 0)) {
         vypis("  prekazka ")
@@ -98,5 +79,4 @@ basic.forever(function () {
         vypis("  normal ")
         basic.pause(100)
     }
->>>>>>> parent of f7dcba5 (Aktualizovať pxt.json, main.blocks, main.ts)
 })
