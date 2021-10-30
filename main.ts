e16aa5f2ba983c92f80bbf9a9d478b2350edae58
mravec <48887485+mravec@users.noreply.github.com>
2021-10-30 22:28:43 +0200

    aa

--- main.ts
+++ main.ts
@@ -10,6 +10,19 @@ input.onButtonPressed(Button.A, function () {
         . . # . .
         . . # . .
         `)
+    if (prekazka < 20 && (lavyMotor > 0 || pravyMotor > 0)) {
+        vypis("  prekazka ")
+        cuteBot.motors(0, 0)
+        basic.pause(500)
+        cuteBot.motors(-40, -40)
+        basic.pause(1000)
+        cuteBot.motors(19, -42)
+        basic.pause(400)
+        cuteBot.motors(0, 0)
+        basic.pause(500)
+    } else {
+    	
+    }
 })
 function vypis (text: string) {
     s += 1
@@ -39,6 +52,59 @@ radio.onReceivedValue(function (name, value) {
         pravyMotor = value
     }
 })
+cuteBot.IR_callback(function () {
+    basic.pause(200)
+    if (cuteBot.IR_Button(cuteBot.IRButtons.Menu)) {
+        soundExpression.giggle.playUntilDone()
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (cuteBot.IR_Button(cuteBot.IRButtons.Plus)) {
+        soundExpression.happy.playUntilDone()
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (cuteBot.IR_Button(cuteBot.IRButtons.Back)) {
+        soundExpression.hello.playUntilDone()
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (cuteBot.IR_Button(cuteBot.IRButtons.Down)) {
+        soundExpression.spring.playUntilDone()
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # #
+            . . . . .
+            . . . . .
+            `)
+        if (cuteBot.IR_Button(cuteBot.IRButtons.Zero)) {
+            soundExpression.spring.playUntilDone()
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                # # # # #
+                . . . . .
+                . . . . .
+                `)
+        }
+    }
+})
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
     lavyMotor = 0
     pravyMotor = 0
@@ -51,6 +117,8 @@ let pravyMotor = 0
 let lavyMotor = 0
 serial.redirectToUSB()
 radio.setGroup(1)
+cuteBot.singleheadlights(cuteBot.RGBLights.RGB_L, 78, 49, 115)
+cuteBot.singleheadlights(cuteBot.RGBLights.RGB_R, 179, 152, 58)
 basic.showLeds(`
     . . . . .
     . # # # .
@@ -64,19 +132,7 @@ s = 0
 soundExpression.happy.play()
 basic.forever(function () {
     prekazka = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
-    if (prekazka < 30 && (lavyMotor > 0 || pravyMotor > 0)) {
-        vypis("  prekazka ")
-        cuteBot.motors(0, 0)
-        basic.pause(500)
-        cuteBot.motors(-40, -40)
-        basic.pause(1000)
-        cuteBot.motors(19, -42)
-        basic.pause(400)
-        cuteBot.motors(0, 0)
-        basic.pause(500)
-    } else {
-        cuteBot.motors(lavyMotor, pravyMotor)
-        vypis("  normal ")
-        basic.pause(100)
-    }
+    cuteBot.motors(lavyMotor, pravyMotor)
+    vypis("  normal ")
+    basic.pause(100)
 })
